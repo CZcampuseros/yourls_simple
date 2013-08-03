7c24a9fb11134f2639f5cd8e1f320f3a689fd9fa
pdostal <git@pdostal.cz>
2013-08-03 19:58:49 +0200

    config.php removed

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$config = array(
-		'server' => 'localhost',
-		'database' => 'yourls',
-		'username' => 'root',
-		'password' => 'dmeoemoe',
-		'htpass' => '098f6bcd4621d373cade4e832627b4f6'
-	);
-?>
